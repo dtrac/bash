c91d4ceabd22fe13404d82e322ac5975750805d0
Dan Tracey <dtracey@hotmail.co.uk>
2020-03-08 16:55:52 +0000

    Update apache-install-ubuntu.sh

--- apache-install-ubuntu.sh
+++ apache-install-ubuntu.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 
 # Install PHP and Apache
-sudo apt-get install -y php apache2
+sudo apt-get install -y php apache2 git
 
 # Copy the code from the repository
 sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
